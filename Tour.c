9bd28136cdbb74ec2c832e05bafd0230fb98838f
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2022-12-29 18:47:05 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=0;i<t;i++)     {      int a,b,sum;     scanf("%d%d",&a,&b);      sum=a*5+b*7;      printf("%d ",sum);     }
    }

---
+++ Tour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+     int a,b,sum;
+    scanf("%d%d",&a,&b);
+     sum=a*5+b*7;
+     printf("%d
+",sum);
+    }
+      
+}
