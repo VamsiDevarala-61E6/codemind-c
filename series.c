1a79a11eeaaa737b5d7dcff53a9563167c989f07
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2022-12-29 22:25:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,a=1,b=1,c=0,d=0;     scanf("%d",&n);     printf("0 ");     for(i=0;i<n-1;i++)     {         if(i%2!=0)         {         printf("%d ",a);
            c++;
             a=pow(2,c);
            }
             else
             {
              printf("%d ",b);
              
              d++;
              b=pow(3,d);
             }
        }
    }

--- series.c
+++ series.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,a=1,b=2,i;
+    int n,i,a=1,b=1,c=0,d=0;
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
+    printf("0 ");
+    for(i=0;i<n-1;i++)
     {
-        if(i%2==0)
+        if(i%2!=0)
         {
-            
-            printf("%d ",a);
-            a=(a*2)+1; 
+        printf("%d ",a);
+       
+        c++;
+         a=pow(2,c);
         }
-        else
-        {
-            
-            printf("%d ",b);
-            b=(b*3)+2;
-        }
+         else
+         {
+          printf("%d ",b);
+          
+          d++;
+          b=pow(3,d);
+         }
     }
 }
