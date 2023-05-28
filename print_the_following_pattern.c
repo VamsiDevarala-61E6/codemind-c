5f8a7b8cafb6fbabb1c3156d3950b1314fef6d97
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-05-28 12:07:38 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n;i>0;i--)     {         for(int j=1;j<=i;j++)         {             printf("%c ",'A'-1+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,11 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    for(int i=n;i>0;i--)
     {
         for(int j=1;j<=i;j++)
         {
-            printf("%d",j);
+            printf("%c ",'A'-1+i);
         }
         printf("
 ");
