f4b18c9ce3066cdc8813c7a39605eb61eb02b2b1
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-13 15:27:43 +0530

    #include<stdio.h> int fact(int n){     int f=1;     for(int i=n;i>=1;i--){         f=f*i;     }     return f; } int main(){     int t;     scanf("%d",&t);     for(int i=1;i<=t;i++)     {         int n,x,s=0;         scanf("%d",&n);         int temp=n;         while(n!=0){             x=n%10;             s=s+fact(x);             n=n/10;         }         if(s==temp)           printf("Strong ");         else            printf("Not Strong ");     } }

--- Strong_Number.c
+++ Strong_Number.c
@@ -1,32 +1,29 @@
 #include<stdio.h>
-int fact(int n)
-{
+int fact(int n){
     int f=1;
-    for(int i=n;i>0;i--)
-    {
+    for(int i=n;i>=1;i--){
         f=f*i;
     }
     return f;
 }
-int main()
-{
-    int n,x;
-    scanf("%d",&n);
-    int t,fr=0;
-    t=n;
-    while(n!=0)
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
     {
-        x=n%10;
-        fr=fr+fact(x);
-        n=n/10;
-        
-    }
-    if(fr==t)
-    {
-        printf("The number %d is a strong number",t);
-    }
-    else
-    {
-        printf("The number %d is not a strong number",t);
+        int n,x,s=0;
+        scanf("%d",&n);
+        int temp=n;
+        while(n!=0){
+            x=n%10;
+            s=s+fact(x);
+            n=n/10;
+        }
+        if(s==temp)
+          printf("Strong
+");
+        else
+           printf("Not Strong
+");
     }
 }
